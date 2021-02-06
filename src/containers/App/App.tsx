8d35a582baeec70881a21d71486bf8bac3ee2f10
Nic Pearson <nic.s.pearson@gmail.com>
2021-02-06 21:35:45 +0100

    fix: update assets

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 
 import logo from '@app/assets/logo.svg';
-import '@app/containers/App.css';
+
+import './style.css';
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>Under construction.</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/lupine-moon/react.lupinemoon"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Source code
         </a>
       </header>
     </div>
--- test/unit/containers/App.test.tsx
+++ test/unit/containers/App.test.tsx
@@ -6,6 +6,6 @@ import App from '@app/containers/App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/under construction/i);
   expect(linkElement).toBeInTheDocument();
 });
